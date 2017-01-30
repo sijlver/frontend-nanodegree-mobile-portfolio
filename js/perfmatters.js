5aeb65961ccf49d6b44cf3bbf4631461ee8d58fc
sijlver <sijlver@mail.ru>
2017-01-30 22:20:26 +0300

    frontend-nanodegree-mobile-portfolio/fix:load img posted in function

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -5,7 +5,13 @@ function logCRP() {
   var stats = document.getElementById("crp-stats");
   stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
 }
-
+function img(){
+	let arr=document.querySelectorAll('.avatar'), arrImg=['img/profilepic.jpg','img/my.jpg','views/images/pizzeria.jpg'];
+	Array.from(arr).forEach((name,i)=>{
+		name.setAttribute('src', arrImg[i]);
+	});
+}
 window.addEventListener("load", function(event) {
   logCRP();
+  img();
 });
